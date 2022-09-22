c6a0e14e0dbd96bbce10eecbccb223418ec38599
Bekzod1999 <developmentfirm1999@gmail.com>
2022-09-22 10:40:14 +0500

    03

--- file03.py
+++ file03.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    k = []
+    for line in data:
+        if line.isdigit():
+            k += line
+        # print(line)
+    return k
     
 # Read data from file
+f = open('txt_file/data03.txt').read()
+
+print(main(f))
