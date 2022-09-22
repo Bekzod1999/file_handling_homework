b0fffe4460bf83147c74f50cd5e389d0a36cf564
Bekzod1999 <developmentfirm1999@gmail.com>
2022-09-22 10:57:52 +0500

    05

--- file05.py
+++ file05.py
@@ -1,3 +1,7 @@
+# from curses.ascii import isdigit
+# from unicodedata import digit
+
+
 def main(data:str):
     """
     The data is from the file. Find the number of digital and str(non-digital) data and return as list type.
@@ -6,5 +10,21 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    k = []
+    d = 0
+    s = 0
+    for digitt in data:
+        if digitt.isdigit():
+            d += 1
+        else:
+            s += 1
+    k.append(d)
+    k.append(s)
+    return k
+
+f = open('txt_file/data05.txt').read()
+
+print(main(f))
+
+
 # Read data from file
