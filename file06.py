a6df8563a92c59547898a6890ee7b9413de96609
Bekzod1999 <developmentfirm1999@gmail.com>
2022-09-22 14:05:13 +0500

    06

--- file06.py
+++ file06.py
@@ -1,3 +1,41 @@
+# def main(data:str):
+#     """
+#     The data is from the file. Find the each row length and return as list type.
+#     Args:
+#         data: str
+#     Returns:
+#         list: return answer
+#     """
+#     k = []
+#     lines = 0
+#     for line in data:
+#         if line == '\n':
+#             lines += 1
+#             break
+#         # print(lengg)
+#     return lines
+# # Read data from file
+
+# f = open('txt_file/data06.txt').read()
+
+# print(main(f))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def main(data:str):
     """
     The data is from the file. Find the each row length and return as list type.
@@ -6,5 +44,12 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
-# Read data from file
+    k = []
+    lines = 0
+    for line in data:
+        k.append(len(line)-1)
+    return k
+
+f = open('txt_file/data06.txt').readlines()
+
+print(main(f))
