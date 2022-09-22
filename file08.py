b7cc0a0f09cbce69d9811b10a817984b508171da
Bekzod1999 <developmentfirm1999@gmail.com>
2022-09-22 11:16:29 +0500

    08

--- file08.py
+++ file08.py
@@ -6,5 +6,20 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    k = []
+    s = 0
+    for line in data:
+        if line.isdigit():
+            k += line
+
+    for summ in k:
+        if s < int(summ):
+            s = int(summ)
+        
+    return s
 
 # Read data from file
+
+f = open('txt_file/data08.txt').read()
+
+print(main(f))
